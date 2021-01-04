dec09b01d4e49f47dd04b00699dca1d47c556690
m-vojjala <mounikavojjala@gmail.com>
2021-01-04 09:37:40 -0500

    test

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,12 +4,12 @@ export default function Education(){
   return <section className="education">
     <h3 className="education-header">EDUCATION</h3>
     <div className="education-one">
-    <h5 style={{fontSize:'20px'}}>Light House Labs - January, 2021</h5>
-    <h5 style={{fontSize:'20px'}}><strong>Diploma in Web Development</strong></h5>
+    <h5>Light House Labs - January, 2021</h5>
+    <h5><strong>Diploma in Web Development</strong></h5>
     </div>
     <div className="education-two">
-    <h5 style={{fontSize:'20px'}}>Jawaharlal Nehru Institute of Technology(India) - June, 2014</h5>
-    <h5 style={{fontSize:'20px'}}><strong>Bachelors in Computer Science</strong></h5>
+    <h5 >Jawaharlal Nehru Institute of Technology(India) - June, 2014</h5>
+    <h5><strong>Bachelors in Computer Science</strong></h5>
     </div>
   </section>
 }  
