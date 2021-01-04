abaec535a8ddf0bb931c8b3940c0e5f9fc23d2a1
m-vojjala <mounikavojjala@gmail.com>
2021-01-04 10:26:02 -0500

    css changed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,10 +3,10 @@ import "./Skills.css"
 export default function Skills() {
   return <section className="skills">
     <h3 className="skills-head">SKILLS</h3>
-    <h5 style={{ fontFamily: "'Oxygen', sans-serif",fontSize: '30px' }}>HTML/CSS | SASS | Bootstrap | Javascript | jQuery | Ajax | React | Node JS |
+    <h5 style={{ fontFamily: "'Oxygen', sans-serif",fontSize: '30px'}}>HTML/CSS | SASS | Bootstrap | Javascript | jQuery | Ajax | React | Node JS |
    <hr />
      Express | Postgres | Git version control </h5>
     <hr />
-    <h5><strong>Spoken Languages</strong>: English, Hindi, Telugu</h5>
+    <h5 style={{ fontFamily: "'Oxygen', sans-serif",fontSize: '30px'}}><strong>Spoken Languages</strong>: English, Hindi, Telugu</h5>
   </section>
 }
