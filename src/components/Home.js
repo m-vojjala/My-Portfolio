46352535566825e7bc6f0fa6ac9b7eb227fede87
m-vojjala <mounikavojjala@gmail.com>
2021-02-01 18:02:24 -0500

    corrected git url

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ export default function Home(){
     <h3 className="name">MOUNIKA VOJJALA</h3>
     <ul className="icons"></ul>
     <a href="https://www.linkedin.com/in/mounika-vojjala-a157a98a/"  target="_blank" rel="noopener noreferrer"><i  class="fab fa-linkedin-in fa-4x" ></i></a>
-    <a href="https://github.com"  target="_blank" rel="noopener noreferrer"><i  class="fab fa-github fa-4x" ></i></a>
+    <a href="https://github.com/m-vojjala"  target="_blank" rel="noopener noreferrer"><i  class="fab fa-github fa-4x" ></i></a>
     </Cell>
     </Grid>
   </section>
